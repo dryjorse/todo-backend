78b568eb452eba2a5f14018790a5348396fdaa3e
dryjorse <kozoevabdulazim@gmail.com>
2025-04-11 15:27:29 +0600

    vercel

--- src/models/index.js
+++ src/models/index.js
@@ -2,20 +2,21 @@ const { Sequelize } = require("sequelize");
 const TodoModel = require("./todo");
 
 const sequelize = new Sequelize(
-  "postgres://postgres.rtvewaypnrvvmsfrhygv:fbWwBfUPrWuWkm8h@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x",
+  process.env.POSTGRES_URL,
+  // "postgres://postgres.rtvewaypnrvvmsfrhygv:fbWwBfUPrWuWkm8h@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x",
   // process.env.POSTGRES_DATABASE || "tododb",
   // process.env.POSTGRES_USER || "postgres",
   // process.env.POSTGRES_PASSWORD || "postgres",
   {
-    host: process.env.POSTGRES_HOST || "localhost",
+    // host: process.env.POSTGRES_HOST || "localhost",
     dialect: "postgres",
     dialectModule: require("pg"),
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // важный момент для Supabase
-      },
-    },
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // важный момент для Supabase
+    //   },
+    // },
   }
 );
 
